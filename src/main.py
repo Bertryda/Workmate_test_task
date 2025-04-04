a13ac41b2ecc1e062106b4d42dc767f9924d6987
bertruda <isenkokirill08@gmail.com>
2025-04-04 20:41:40 +0300

    create_functions_for_logging_multiple_files

--- src/main.py
+++ src/main.py
@@ -42,12 +42,59 @@ def parse_log_line(line: str) -> Tuple[Optional[str], Optional[str]]:
     """
     
     cache = LOG_PATTERN.match(line.strip())
+    
     if cache:
         level = cache.group("level").upper()
         handler = cache.group("handler")
         return level, handler
     return None, None
 
+
+async def parse_log_file(file_path: str) -> Dict[str, Dict[str, int]]:
+    """
+    Функция асинхронно обрабатывает файл логов и возвращает
+    полученную статистику
+
+    Args:
+        file_path (str): путь к файлу, который анализируется
+
+    Returns:
+        Dict[str, Dict[str, int]]: возвращается словарь с вложенными 
+        словарями для каждого уровня
+    """
+    data = {}
+
+    async for line in read_file_lines(file_path):
+        level, handler = parse_log_line(line)
+        if handler and handler not in data:
+            data[handler] = {}
+        if level in LOG_LEVELS and handler:
+            stats[handler][level] += 1
+            
+    return data
+
+
+async def parse_log_files(
+        file_path_list: List[str],
+    ) -> List[Dict[str, Dict[str, int]]]:
+    """
+    Функция для асинхронной обработки нескольких файлов логов
+
+    Args:
+        file_path_list (List[str]): список путей к файлам
+
+    Returns:
+        List[Dict[str, Dict[str, int]]]: результат тот же что и в функции
+        parse_log_file но несколько значений
+    """
+    result = [parse_log_file(file_path) for file_path in file_path_list]
+    return await asyncio.gather(*result)
+
+
+
+
+    
+        
 
 
             
