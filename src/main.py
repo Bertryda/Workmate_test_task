3f87f60d0f952b04757a4ebcb50349da9ec9a026
bertruda <isenkokirill08@gmail.com>
2025-04-04 15:57:51 +0300

    create_func_read_file

--- src/main.py
+++ src/main.py
@@ -6,3 +6,19 @@ from collections import defaultdict
 from typing import Dict, List, Tuple, Optional, AsyncIterable
 
 
+async def read_file_lines(file_path: str) -> AsyncIterable[str]:
+    """
+    Функция асинхронно и и построчно читает файл, доступ 
+    к которому происходит через file_path
+
+    Args:
+        file_path (str): Путь к файлу с логами
+
+    Returns:
+        AsyncIterable[str]: Возвращает корутину после обработки файла
+    """
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            yield line
+            
+            
